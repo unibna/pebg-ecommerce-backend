e343c3a76cb3dba68e469067edd63d0cb64fef80
Ryan <nguyenduylan711@gmail.com>
2024-08-05 11:32:46 +0700

    update order status enum

--- order/enums.py
+++ order/enums.py
@@ -3,7 +3,6 @@ from base.enums import BaseEnum
 
 class OrderStatusEnum(str, BaseEnum):
     DRAFT = 'Draft'
-    PAYMENT_PENDING = 'Payment Pending'
     PAYMENT_COMPLETED = 'Payment Completed'
     PROCESSING = 'Processing'
     DELIVERING = 'Delivering'
